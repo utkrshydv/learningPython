f9480d7ca4f2f44a6be042947b8f974581cc11e0
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-07-21 22:48:52 +0530

    Create revision/lab1.py

---
+++ revision/lab1.py
@@ -0,0 +1,45 @@
+#fstring
+
+#name = input("enter your name: ")
+#birthyear = input("enter your birthyear: ")
+#age = 2025 - int(birthyear)
+#lastname = input("enter last name: ")
+#print(f"hello {name} {lastname} youre {age} years old")
+
+#type conversion
+
+#a = int(input("enter a number: "))
+#b = int(input("enter another number: "))
+#print(f'sum: {a+b}')
+
+#price = int(input("enter a price: "))
+#discounted = price - float(price)*0.15
+#print(f'discounted price : {discounted}')
+
+#base, height = map(int, input("Enter base and height: ").split(", "))
+#print(f'area : {0.5*base*height}')
+
+#num = int(input("enter a number: "))
+
+#if num%2 == 0:
+#    print("even")
+#else:
+#    print("odd")
+
+#num = int(input("enter a number"))
+#print(f'square: {num**2}, cube: {num**3}, quad: {num**4}')
+
+#name, roll = input("enter name and roll: ").split(", ")
+#print(f'name: {name}, roll: {roll}')
+
+
+#a, b, c = map(int, input("enter a, b and c: ").split(", "))
+#print(f'sum: {a+b+c}')
+
+#print(int("43")+int("20"))
+
+#a, b = input("enter numbers: ").split(", ")
+
+#average =( int(a) + int(b) )/2
+#print(average)
+#print(a+b)
